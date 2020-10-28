c0ffc186077c245c394c98d41e5be5731daa5ce8
Henry Gordon <henrytgordon@gmail.com>
2020-10-28 16:39:52 -0400

    Show a link to create an album when there are no albums in the DB

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -6,13 +6,18 @@
 @include('common.status')
 
 <div class="content">
-    @foreach ($albums as $album)
-    <div class="album">
-        <a href="/album/{{$album->id}}/edit">
-            <img src="storage/{{$album->image}}">
-            <label>{{$album->artist}} - {{$album->title}}</label>
-        </a>
-    </div>
-    @endforeach
+    @if (count($albums) > 0)
+        @foreach ($albums as $album)
+        <div class="album">
+            <a href="/album/{{$album->id}}/edit">
+                <img src="storage/{{$album->image}}">
+                <label>{{$album->artist}} - {{$album->title}}</label>
+            </a>
+        </div>
+        @endforeach
+    @else
+        <p>There are no albums in the database.
+        You can create one <a href="/album/create">here</a>.</p>
+    @endif
 </div>
 @endsection
