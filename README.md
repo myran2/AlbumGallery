b276b7138b217b0568ba991de1c8aec7dbc33a13
Henry Gordon <henrytgordon@gmail.com>
2020-10-28 20:21:42 -0400

    Readme updates and add initial albums to database

--- README.md
+++ README.md
@@ -8,14 +8,14 @@ Albums can also be edited, deleted, and created.
 2. Install [Composer](https://getcomposer.org/download).
 2. Manually create a MySQL database for the project (default name is `albumgallery` but this can be specified in `.env`)
 3. From the projects root run `cp .env.example .env`
-4. Configure your `.env`
-5. Run `sudo composer update` from the projects root folder
-6. From the projects root folder run `sudo chmod -R 755 ../AlbumGallery`
+4. Configure your `.env`. The default app_ values should be correct for a local setup, but make sure the MYSQL_ ones match your setup.
+5. Run `sudo composer update` from the projects root folder. This installs the default laravel dependencies (kinda takes a while).
+6. From the projects root folder run `sudo chmod -R 755 ../AlbumGallery` or `AlbumGallery` should be whatever you named the folder that you cloned the repo into.
 7. From the projects root folder run `php artisan key:generate`. This generates a unique key.
 8. From the projects root folder run `php artisan migrate`. This initializes the database.
 9. From the projects root folder run `php artisan storage:link`. This creates a symlink between the storage/app/public and public/storage directories.
     If images aren't appearing in the gallery after upload, it's because this symlink isn't in place.
-10. From the projects root folder run `composer dump-autoload`. This installs the default laravel dependencies (kinda takes a while).
+10. From the projects root folder run `composer dump-autoload`. This tells laravel which dependencies to load
 
 #### View the Project in Browser
 1. From the projects root folder run `php artisan serve`
--- database/migrations/2020_10_27_175946_create_albums_table.php
+++ database/migrations/2020_10_27_175946_create_albums_table.php
@@ -21,6 +21,59 @@ class CreateAlbumsTable extends Migration
             $table->string('image');
             $table->timestamps();
         });
+
+        DB::table('albums')->insert(array(
+            array(
+                "title" => "Morbid Stuff",
+                "artist" => "Pup",
+                "image" => "9OocwsRGEITbaCmom6G1YWO8KyVNvldkm1fuGec8.jpeg",
+            ),
+            array(
+                "title" => "10",
+                "artist" => "Tricot",
+                "image" => "asWQbotBOs5jHGnqZRezNn33c8QsTQvRcfHcwPbh.jpeg",
+            ),
+            array(
+                "title" => "No Drum and Bass in the Jazz Room",
+                "artist" => "Clever Girl",
+                "image" => "D8OBN2IarKOsKyiBfWdL0saGcOllFAHecAG7ZXtl.jpeg",
+            ),
+            array(
+                "title" => "Lift Your Skinny Fists Like Antennas to Heaven",
+                "artist" => "Godspeed You! Black Emperor",
+                "image" => "fUQl7D6tU4DrAmT3BgRBgkS710sFm7rEQw9tRtZc.jpeg",
+            ),
+            array(
+                "title" => "Happiness",
+                "artist" => "Dance Gavin Dance",
+                "image" => "nEQ8OzE3XUQOBdeuYGDFnPT9ZicAGS9d1Z2c6raZ.jpeg",
+            ),
+            array(
+                "title" => "Holy Ghost",
+                "artist" => "Modern Baseball",
+                "image" => "ru6ini4b95n2Pcg7x56LO94poh1ttR6sPPEyNZG7.jpeg",
+            ),
+            array(
+                "title" => "Run",
+                "artist" => "Prawn",
+                "image" => "VGWHkrWiuEpc89ZJQR6r7DD3UO43BRmPkhOfXpWP.jpeg",
+            ),
+            array(
+                "title" => "Parting the Sea Between Brightness and Me",
+                "artist" => "Touché Amoré",
+                "image" => "WBzsnSDzHmNTAC85ogTjclv5KuHRlItJs3ZDtHcA.jpeg",
+            ),
+            array(
+                "title" => "Program Music II",
+                "artist" => "Kashiwa Daisuke",
+                "image" => "Y3YsRC0ko2RdA0GcZ7rALKGD5QvynzGUaXISi15J.jpeg",
+            ),
+            array(
+                "title" => "Worry",
+                "artist" => "Jeff Rosenstock",
+                "image" => "yucIf2UXiOrvx1BpniG32nPHaOJaUG8V6iLk9uNL.jpeg",
+            ),
+        ));
     }
 
     /**
